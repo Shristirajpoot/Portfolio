90a9fd3ea6c9b57c6e29711c81f926540713733b
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2026-01-12 01:39:20 +0530

    Set dynamic routing for send API

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -3,6 +3,7 @@ import { config } from "@/data/config";
 import { Resend } from "resend";
 import { z } from "zod";
 
+export const dynamic = "force-dynamic";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const Email = z.object({
